ab682bce6053892663a3b6fe801ff04dcd73b7cb
Kirill Shmilovich <kshmilov@gmail.com>
2019-08-16 13:49:19 -0500

    Added some utils, added Graphlets class

--- graphlets/graphlets.py
+++ graphlets/graphlets.py
@@ -4,32 +4,43 @@ Python package for computing graphlets
 
 Handles the primary functions
 """
-import os
-ORCA_PATH = os.path.abspath("../orca/orca.exe")
+from .utils import *
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+import numpy as np
+from joblib import Parallel, delayed
+import subprocess
 
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+class Graphlets():
+
+    def __init__(self, X, dims=None, metric="euclidean", depth=5):
+        "X is (time_steps, num_structures, p_dims)"
+        self.X = X
+        self.metric = metric
+        self.depth = depth
+        self.dims = dims
+    
+    def decompose(self,G,t):
+        in_file = f".edgelist_{t}.dat"
+        out_file = f".edgelist_{t}.out"
+
+        f = open(in_file,"wb")
+        f.write(f"{nx.number_of_nodes(G)} {nx.number_of_edges(G)}\n".encode())
+        nx.write_edgelist(G,f,data=False)
+        f.close()
+
+        command = [ORCA_PATH,"node",str(self.depth),in_file,out_file]
+        subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        decomposition = np.loadtxt(out_file)*weights
+        command = ["rm",out_file,in_file]
+        subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return decomposition
+    
+    def compute(self,r_cut, reduce=True, n_jobs=-1):
+        G_all = np.asarray(Parallel(n_jobs=n_jobs)(delayed(self.decompose)(compute_graph(x, r_cut, dims=self.dims, metric=self.metric),t) for t,x in enumerate(self.X)))
+        if reduce is True:
+            return G_all
+        else:
+            return G_all
+
+    
