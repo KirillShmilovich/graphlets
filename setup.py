dbe8860f0ac2f85c6356940fa1dde3a3cc107ff1
Kirill Shmilovich <kshmilov@gmail.com>
2019-08-26 09:25:54 -0500

    fixing

--- setup.py
+++ setup.py
@@ -49,7 +49,8 @@ setup(
 
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-    ext_modules = [orca_module]
+    #ext_modules = [orca_module]
+    
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
     # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
